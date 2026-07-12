8addc7d73d4a7f0e6231c4b7b981dadc75f05da1
Vishva P <vishvavichu13@gmail.com>
2026-07-12 22:39:21 +0530

    Altered the inital UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,30 +11,30 @@ function App() {
       </header>
       <main className="App-main">
         <div className="info-box-div">
-            <div className="info-box">
-                <p>
-                Welcome! This page lets you explore your Letterboxd profile like never before:
-                </p>
-                <ul>
-                <li>Enter your Letterboxd username to get started.</li>
-                <li>View personalized statistics based on the year of your watched movies.</li>
-                <li>Discover top film recommendations tailored to your viewing history.</li>
-                </ul>
-                <p>
-                Dive in and see your movie journey come to life!
-                </p>
-            </div>
+          <div className="info-box">
+            <p>
+              Welcome! This page lets you explore your Letterboxd data like never before:
+            </p>
+            <ul>
+              <li>Upload your Letterboxd export ZIP file to get started.</li>
+              <li>View personalized statistics based on your watched movies.</li>
+              <li>Discover top film recommendations tailored to your viewing history.</li>
+            </ul>
+            <p>
+              Upload your export and see your movie journey come to life!
+            </p>
+          </div>
         </div>
-        {/* Username input section below the info box */}
+
         <div className="input-section">
-            <input
-            type="text"
-            placeholder="Enter your Letterboxd username"
-            className="username-input"
-            />
-            <button className="fetch-button">Fetch Details</button>
+          <input
+            type="file"
+            accept=".zip"
+            className="zip-input"
+          />
+          <button className="fetch-button">Analyze ZIP</button>
         </div>
-        </main>
+      </main>
     </div>
   );
 }
